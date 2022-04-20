901c43064733fdf0a545e301afd9de6cc2fb015f
2B <102602452+A29S@users.noreply.github.com>
2022-04-20 20:55:20 +0100

    Add files via upload

---
+++ bin2dec.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include "pilha.h"
+#include "conversor.h"
+
+/* Para compilar: gcc -g -Wall bin2dec.c conversor.c pilha.c -o bin2dec */
+
+int main()
+{
+  pilha *p = pilha_nova();
+
+  if (get_bin(p))
+    print_dec(p);
+
+  pilha_apaga(p);
+
+  return 0;
+}
